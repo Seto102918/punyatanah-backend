3c816cd17e55460fb831f5330f48fb7d0ed33fb0
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-14 06:40:57 +0700

    added command to forward the tables

--- src/database/connection.js
+++ src/database/connection.js
@@ -15,6 +15,9 @@ const db = {
   connect,
   Sequelize,
   review: require("../models/review")(Sequelize, connect),
+  tanah_image: require("../models/tanah_image")(Sequelize, connect),
+  user: require("../models/user")(Sequelize, connect),
+  tanah: require("../models/tanah")(connect, Sequelize),
 };
 
 module.exports = db;
