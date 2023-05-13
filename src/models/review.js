ad59b58742dd90eba8a2676d8c24a403e7693860
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-14 06:41:23 +0700

    fixed review.js

--- src/models/review.js
+++ src/models/review.js
@@ -1,24 +1,30 @@
 const review = (Sequelize, sequelize) => {
-  return sequelize.define("review", {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
+  return sequelize.define(
+    "review",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      id_user: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
     },
-    id_user: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-    role: {
-      type: Sequelize.STRING(10),
-      allowNull: false,
-    },
-    message: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: true,
+    }
+  );
 };
 
 module.exports = review;
