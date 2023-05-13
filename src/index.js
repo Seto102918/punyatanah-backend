461d4e5129b14be71c5d5c4b20544d0d5768e8f5
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-13 23:26:55 +0700

    seque try to connect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connection = require("./database/connection");
 
 const router = require("./routes/router");
 
@@ -12,6 +13,9 @@ app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
 
 app.use("/", router);
+
+connection.connect.sync({ force: true });
+console.log("a");
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("Server Running " + process.env.SERVER_PORT);
