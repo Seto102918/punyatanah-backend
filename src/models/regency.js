83884a43a13fb78c3c5610069088a21a61b9b853
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-14 07:18:12 +0700

    created table regency.js

---
+++ src/models/regency.js
@@ -0,0 +1,21 @@
+const regency = (Sequelize, sequelize) => {
+  return sequelize.define(
+    "regency",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: true,
+        autoIncrement: true,
+      },
+      regency_name: {
+        type: Sequelize.STRING(50),
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+};
+
+module.exports = regency;
